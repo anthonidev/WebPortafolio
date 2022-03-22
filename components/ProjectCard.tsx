d60fb666777b1f2387ad8993a6ef29707293be3b
anthoni portocarrero rodriguez <61163166+anthoni-gmu@users.noreply.github.com>
2022-03-22 18:01:53 -0500

    projects navbar

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -20,31 +20,48 @@ const ProjectCard: FunctionComponent<{
     key_techs,
   },
 }) => {
+    const [showDetail, setShowDetail] = useState(false)
     return (
       <div>
 
-        <img src={image_path} alt={name} className="cursor-pointer" />
+        <img src={image_path} alt={name} className="cursor-pointer" onClick={() => setShowDetail(true)} />
         <p className="my-2 text-center" >{name}</p>
 
-        <div className="grid md:grid-cols-2">
-          <img src={image_path} alt={name} />
-          <a href={github_url}>
-          <AiFillGithub /> <span>Github</span>
 
-          </a>
-          <a href={github_url}>
-          <AiFillProject /> <span>Project</span>
+        {
+          showDetail && (
+            <div className="absolute top-0 left-0 grid z-10 p-2 dark:bg-dark-100 md:grid-cols-2 h-auto w-full gap-x-12 text-black bg-gray-100 dark:text-white">
+              <div>
+                <img src={image_path} alt={name} />
+                <div className="flex justify-center my-4 space-x-3">
+                  <a href={github_url} className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200">
+                    <AiFillGithub /> <span>Github</span>
 
-          </a>
-        </div>
+                  </a>
+                  <a href={github_url} className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200">
+                    <AiFillProject /> <span>Project</span>
+                  </a>
+                </div>
 
-        <div>
-          <h2>{name}</h2>
-          <h3>{description}</h3>
-          {
-            key_techs.map(key_tech)
-          }
-        </div>
+              </div>
+              <div>
+                <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
+                <h3 className="mb-3 font-medium">{description}</h3>
+                <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
+                {
+                  key_techs.map(tech => <span className="px-2 py-2 bg-gray-200 dark:bg-dark-200" key={tech}>{tech}</span>)
+                }
+                </div>
+               
+
+                <button
+                  className="absolute top-3 right-3 rounded-full p-1 focus:outline-none bg-gray-200 dark:bg-dark-200 "
+                  onClick={() => setShowDetail(false)}><MdClose size={30} /></button>
+              </div>
+            </div>
+          )
+
+        }
 
       </div>
     )
---
+++ pages/projects.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import ProjectCard from "../components/ProjectCard";
+import { projects as projectsData } from "../data";
+import { Category } from "../type";
+import ProjectsNavbar from "../components/ProjectsNavbar";
+const Projects = () => {
+
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState("all");
+
+  const handlerFilterCategory = (category: Category | "all") => {
+
+    if (category === "all") {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+
+    const newArray = projectsData.filter((project) =>
+      project.category.includes(category)
+    );
+    setProjects(newArray);
+    setActive(category);
+  };
+  return (
+    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
+      <ProjectsNavbar handlerFilterCategory={handlerFilterCategory} />
+
+      <div className="grid  grid-cols-12 gap-4 my-3 relative ">
+
+        {
+          projects.map(project => (
+            <div className="col-span-12 sm:col-span-6  lg:col-span-4 p-2 dark:bg-dark-200 bg-gray-200 rounded-lg" key={project.name}>
+              <ProjectCard project={project} />
+            </div >
+          ))
+        }
+      </div>
+    </div>
+
+  );
+};
+
+export default Projects;
